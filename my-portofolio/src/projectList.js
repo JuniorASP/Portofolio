d1511d5a94702ec2a6e75d98d81c3aba3ddc2d25
Junior <Junior-suarezperalta@hotmail.com>
2023-12-10 20:29:25 +0100

    .

--- my-portofolio/src/projectList.js
+++ my-portofolio/src/projectList.js
@@ -4,14 +4,14 @@ export const projectList =[
 
     {   id: 0,
         Name: 'Flappy Bird',
-        Image: '/overig/img3.jpg',
+        Image: '../overig/img3.jpg',
         Skills: 'Javascript, Html, Css',
         Github: 'https://github.com/JuniorASP/Flappy-bird'
     },
 
     {   id: 1,
         Name: 'Doodle Jump',
-        Image: '/overig/img4.png',
+        Image: '../overig/img4.png',
         Skills: 'Javascript, Html, Css',
         Github: "https://github.com/JuniorASP/Doodle-Jump"
         
@@ -19,7 +19,7 @@ export const projectList =[
 
     {   id: 2,
         Name: 'Employe Directory',
-        Image: '/overig/img5.jpg',
+        Image: '../overig/img5.jpg',
         Skills: 'Javascript, Html,Css',
         Github: "https://github.com/JuniorASP/Techproyect-8"
 
@@ -29,7 +29,7 @@ export const projectList =[
     {
         id: 3,
         Name: ' Webshop',
-        Image: '/overig/img1.jpg',
+        Image: '../overig/img1.jpg',
         Skills: 'React,Javascript, Html,Css',
         Github: "https://github.com/JuniorASP/my-webshop-react"
     },
@@ -37,7 +37,7 @@ export const projectList =[
     {
         id: 4,
         Name: 'Flight Widget',
-        Image: '/overig/img2.webp',
+        Image: '../overig/img2.webp',
         Skills: 'Javascript,Css',
         Github: "https://github.com/JuniorASP/flight-widget"
 
